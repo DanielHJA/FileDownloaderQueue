e0bc5ab3340cd3f17ba60edff678d772d292168b
Daniel <daniel.hjartstrom@gmail.com>
2020-03-13 09:11:55 +0100

    Initial Commit

---
+++ CustomOperations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomOperations
+//
+//  Created by Daniel Hjärtström on 2020-03-13.
+//  Copyright © 2020 Daniel Hjärtström. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
