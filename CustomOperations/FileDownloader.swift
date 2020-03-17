9047f02ca7ca0465d5e8c9268fc92a5de2ee93d7
Daniel <daniel.hjartstrom@gmail.com>
2020-03-17 10:32:39 +0100

    Storing resume data locally

--- CustomOperations/FileDownloader.swift
+++ CustomOperations/FileDownloader.swift
@@ -45,15 +45,17 @@ class FileDownloader: NSObject, URLSessionDelegate, URLSessionDownloadDelegate {
     }
     
     private func setResumeDataIfExist() {
-        if let resumeData = object?.resumeData {
+        guard let object = object else { return }
+        if let resumeData = Storage.shared.fetchResumeDataForFile(object.name) {
             downloadTask = session?.downloadTask(withResumeData: resumeData)
         }
     }
     
     func suspendDownloadTask() {
-        object?.isRunning = false
+        guard let object = object else { return }
+        object.isRunning = false
         downloadTask?.cancel(byProducingResumeData: { (data) in
-            self.object?.resumeData = data
+            Storage.shared.storeResumeData(data, name: object.name)
         })
     }
     
@@ -65,11 +67,11 @@ class FileDownloader: NSObject, URLSessionDelegate, URLSessionDownloadDelegate {
                 self?.delegate?.updateProgress(progress)
             }
         }
-        
     }
     
     func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
-        
+        guard let object = object else { return }
+        Storage.shared.saveFileFromTemporaryLocation(location, filename: object.name)
     }
     
     func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
@@ -80,6 +82,70 @@ class FileDownloader: NSObject, URLSessionDelegate, URLSessionDownloadDelegate {
         print("Download completed")
         object?.isRunning = false
         finishDelegate?.downloadOperationDidFinish()
+    }
+    
+}
+
+class Storage {
+    static let shared = Storage()
+    
+    private var manager = FileManager.default
+    
+    private var defaultDirectory: URL? {
+        return manager.urls(for: .documentDirectory, in: .userDomainMask).first
+    }
+    
+    func saveFileFromTemporaryLocation(_ tempLocation: URL, filename: String) {
+        guard let defaultDirectory = defaultDirectory else { return }
+        let newLocation = defaultDirectory.appendingPathComponent(filename)
+        
+        if fileExist(filename, location: defaultDirectory) {
+            print("File exists")
+        } else {
+            do {
+                try manager.copyItem(at: tempLocation, to: newLocation)
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+    func fileExist(_ filename: String, location: URL?) -> Bool {
+        guard let filePath = location?.appendingPathComponent(filename).path else { return false }
+        return manager.fileExists(atPath: filePath)
+    }
+    
+    func fetchResumeDataForFile(_ filename: String) -> Data? {
+        guard let defaultDirectory = defaultDirectory else { return nil }
+        let resumeDataDirectoryLocation = defaultDirectory.appendingPathComponent("resumedata")
+        let resumeDataLocation = resumeDataDirectoryLocation.appendingPathComponent("\(filename).resumedata")
+        do {
+            let data = try Data(contentsOf: resumeDataLocation)
+            return data
+        } catch {
+            print(error)
+        }
+        return nil
+    }
+
+    func storeResumeData(_ data: Data?, name: String) {
+        guard let defaultDirectory = defaultDirectory else { return }
+        let resumeDataDirectoryLocation = defaultDirectory.appendingPathComponent("resumedata")
+        let resumeDataLocation = resumeDataDirectoryLocation.appendingPathComponent("\(name).resumedata")
+        
+        if fileExist("resumedata", location: defaultDirectory) {
+            do {
+                try data?.write(to: resumeDataLocation)
+            } catch {
+                print(error)
+            }
+        } else {
+            do {
+                try manager.createDirectory(at: resumeDataDirectoryLocation, withIntermediateDirectories: false, attributes: [:])
+            } catch {
+                print(error)
+            }
+        }
     }
     
 }
